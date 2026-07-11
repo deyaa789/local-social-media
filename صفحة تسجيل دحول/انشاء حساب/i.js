3535b8cbabfec9f2b923218cbeed4d13cbe7d8a4
Deyaa <yabdynythyaty@gmail.com>
2026-07-12 02:41:14 +0300

    adding local storage to create acc

--- صفحة تسجيل دحول/انشاء حساب/i.js
+++ صفحة تسجيل دحول/انشاء حساب/i.js
@@ -2,17 +2,28 @@ const email = document.getElementById(`emailValue`);
 const pw = document.getElementById(`pwValue`);
 const button = document.getElementById(`button`);
 
-let usersData=[]
+let usersArray = JSON.parse(localStorage.getItem("usersData")) || [];
+
 button.addEventListener(`click`, (event) => {
   event.preventDefault();
+
   if (pw.value === `` || email.value === ``) {
     return;
   }
-  if (!email.value.includes(`@gmail`)) {
+  if (!email.value.endsWith(`@gmail.com`)) {
     return;
   }
 
   if (pw.value.length < 8) {
+    return;
+  }
+
+  const isExisting = usersArray.find(function (user) {
+    return user.email === email.value;
+  });
+
+  if (isExisting) {
+    window.alert(`this user exit wanna log in?`);
     return;
   }
   addNewUser();
@@ -21,13 +32,15 @@ button.addEventListener(`click`, (event) => {
 function addNewUser() {
   class users {
     constructor(email, pw) {
-
-      this.email= email;
+      this.email = email;
       this.pw = pw;
     }
   }
-  let newUser = new users(email.value,pw.value)
-  usersData.push(newUser);
 
-  console.log(usersData)
+  let newUser = new users(email.value, pw.value);
+  usersArray.push(newUser);
+
+  localStorage.setItem(`usersData`, JSON.stringify(usersArray));
+
+  window.alert(localStorage.getItem("usersData"));
 }
